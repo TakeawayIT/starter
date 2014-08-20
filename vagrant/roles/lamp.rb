84522fc0879e5089da60165e5545fa45dbf8ca2c
Ali Bahman <abahman@takeawayit.com>
2014-08-20 13:30:37 +0100

    Provisioning MySql server and client

--- vagrant/roles/lamp.rb
+++ vagrant/roles/lamp.rb
@@ -6,11 +6,15 @@ override_attributes(
         "contact" => 'abn@webit4.me',
         "listen_ports" => ["80", "443"],
         "default_modules" => ["status","info","ssl","php5"]
+    },
+    "mysql" => {        
+        "server_root_password" => 'pass',                        
     }
 )
 
 # Run list function we mentioned earlier
 run_list(         
     "recipe[apache2]",    
-    "recipe[php]"    
+    "recipe[mysql::server]",
+    "recipe[mysql::client]"
 )
