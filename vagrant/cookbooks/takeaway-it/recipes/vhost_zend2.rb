f9061ec8719f4903e4d6c49e77ab61f2cb845798
Ali Bahman <info@takeawayit.com>
2014-08-25 20:10:06 +0100

    Update vhost_zend2.rb

--- vagrant/cookbooks/takeaway-it/recipes/vhost_zend2.rb
+++ vagrant/cookbooks/takeaway-it/recipes/vhost_zend2.rb
@@ -1,6 +1,6 @@
 web_app "vhost_zend2" do
   server_name "zend2.takeawayit.local"
-  server_aliases ["zf2.takeawayit.local"]
+  server_aliases ["zend.takeawayit.local","zf2.takeawayit.local","zf.takeawayit.local"]
   allow_override "all"  
   docroot "/var/www/zend2/public"
   directory_index ["index.php"]
